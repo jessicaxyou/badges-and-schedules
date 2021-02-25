3a6756c10e458f8249e99a51bd7b38b5175dac28
jessica you <jessicaxytan@gmail.com>
2021-02-25 17:30:43 -0500

    commit tree 343cb8ed60291ffb0ccc51356931d577cf5b693f

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,29 @@
-# Write your code here.
+
+
+# badge_maker to return a formatted badge
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(hello)
+    hello.map do | attend |
+        "Hello, my name is #{attend}."
+    end
+end
+
+def assign_rooms(people)
+    people.map.with_index(1) do |attendee, room|
+        "Hello, #{attendee}! You'll be assigned to room #{room}!"
+    end
+end
+
+def printer(order)
+    batch_badge_creator(order).each do |badge|
+        puts badge
+    end
+
+    assign_rooms(order).each do |room|
+        puts room
+    end
+
+end
